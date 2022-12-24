1397c277a3431743d9372b914170563e7ae05547
IiroKarppanen <iiro.karppanen@gmail.com>
2022-12-24 21:41:43 +0200

    final commit

--- server/app.js
+++ server/app.js
@@ -4,50 +4,33 @@ const Violator = require('./models/violator');
 const socketIO = require('socket.io')
 require("dotenv").config();
 
+
 const io = socketIO(5000, {
   cors: {
-    origin: "*",
+    origin: "https://ndz-perimeter.vercel.app",
     methods: ["GET"],
     allowedHeaders: ["content-type"]
   },
 });
 
 
-// Connect to database
+// Connect to mongodb and begin data update loop
 const dbURL = `mongodb+srv://${process.env.MONGOUSERNAME}:${process.env.MONGOPASSWORD}@cluster0.kpgcpls.mongodb.net/perimeterDB?retryWrites=true&w=majority`
 mongoose.set('strictQuery', true);
 mongoose.connect(dbURL).then(() => {updateDB(null)})
 
 
-// Establish socket connection to client(s)
-io.on('connection', () => {
-  Violator.find().sort({ timestamp: -1 })
-    .then(violators => {
-      fetchData.droneData().then(alldrones => {
-        io.emit('data', violators, alldrones)
-      })
-    })
-})
-
 
 function updateDB(lastResponse) {
 
-    // Send data to frontend 
-    Violator.find().sort({ timestamp: -1 })
-      .then(violators => {
-        fetchData.droneData().then(alldrones => {
-          io.emit('data', violators, alldrones)
-        })
-      })
-    
-
     fetchData.droneData().then(res => { 
         
+      // If droneData has changed, update database
       if (JSON.stringify(lastResponse) != JSON.stringify(res) && res != null){
 
         res.map((drone) => {
 
-          // Calcuöate if drone is inside NDZ
+          // Calculate if drone is inside NDZ
           
           const x = drone.positionX[0]
           const y = drone.positionY[0]
@@ -59,23 +42,33 @@ function updateDB(lastResponse) {
   
             fetchData.pilotData(drone.serialNumber[0]).then((pilot) => {
 
-              // Update drone document or create new one if it doesn't exist yet
-              Violator.findOneAndUpdate({droneSerialNumber: drone.serialNumber[0]}, {
-              $set:{
-              timestamp: new Date(), 
-              FirstName: pilot.firstName,
-              LastName: pilot.lastName,
-              PhoneNumber: pilot.phoneNumber,
-              Email: pilot.email},
-              $min:{
-              distance: (distanceFromCenter / 1000).toFixed(3)}},
-              {upsert: true}, (error) => {
-                if (error) {console.log(error)}
-              })  
+              if (pilot != null) {
+                // Update drone document or create new one if it doesn't exist yet
+                Violator.findOneAndUpdate({droneSerialNumber: drone.serialNumber[0]}, {
+                  $set:{
+                  timestamp: new Date(), 
+                  FirstName: pilot.firstName,
+                  LastName: pilot.lastName,
+                  PhoneNumber: pilot.phoneNumber,
+                  Email: pilot.email},
+                  $min:{
+                  distance: (distanceFromCenter / 1000).toFixed(3)}},
+                  {upsert: true}, (error) => {
+                    if (error) {console.log(error)}
+                  })  
+              }
             })        
           }
         })
       }
+
+      // Send data to frontend 
+      Violator.find().sort({ timestamp: -1 })
+      .then(violators => {
+          io.emit('data', violators, res)
+      })
+
+      
       return res;
     })
 
